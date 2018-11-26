9991f77581a114153bebb6e5dfccecacf9c431e5
Alex <climberfields@gmail.com>
2018-11-26 15:45:00 -0600

    url pull and download of page information established. Storing the client information as a variable to allow reusage

--- webscraper.py
+++ webscraper.py
@@ -2,3 +2,10 @@
 from urllib.request import urlopen as uReq
 # bs4 stands for Beautiful soup. Beautiful soup is the function.
 from bs4 import BeautifulSoup as soup
+# my_url is the variable
+my_url = 'https://www.newegg.com/Apple-Laptops-Notebooks/BrandSubChat/ID-1759-32'
+# when running uReq it will obtain the information from an area, it will download the webpage and this makes it a client. Store the client as a variable.
+uClient = uReq(my_url)
+# when you read the page it will dump the information and you may not be able to access it again, use a variable to store it.
+page_html = uClient.read()
+uClient.close()
