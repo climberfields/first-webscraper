93433c364cd930f3c1534fb02899ea4013211fab
Alex <climberfields@gmail.com>
2018-11-26 15:40:16 -0600

    URL and Beautiful Soup imports established

---
+++ webscraper.py
@@ -0,0 +1,4 @@
+# urllib is the dependency, request is the module, always remember dependency first, module next. urlopen is the function
+from urllib.request import urlopen as uReq
+# bs4 stands for Beautiful soup. Beautiful soup is the function.
+from bs4 import BeautifulSoup as soup
